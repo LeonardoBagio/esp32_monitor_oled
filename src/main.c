bfb7d8bb78062d7af33aa905e74404bbe65a30e9
Leonardo Bagio <49789651+LeonardoBagio@users.noreply.github.com>
2020-10-27 19:43:11 -0300

    incluir leitura sensor dht11

--- src/main.c
+++ src/main.c
@@ -1,13 +1,16 @@
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_system.h"
 #include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include "esp_system.h"
 #include "driver/gpio.h"
 #include <u8g2.h>
 #include "u8g2_esp32_hal.h"
-/*#include "sdkconfig.h"
+#include "sdkconfig.h"
 #include <dht.h>
-#include <ultrasonic.h>*/
+#include <ultrasonic.h>
+#include <driver/spi_master.h>
+#include <stdio.h>
+#include <string.h>
 
 #define TRIG            GPIO_NUM_0
 #define ECHO            GPIO_NUM_2
@@ -17,12 +20,16 @@
 #define BUTTON          GPIO_NUM_16
 #define EIXO_X          10
 
-//static const dht_sensor_type_t sensor_type = DHT_TYPE_DHT11;
+static const dht_sensor_type_t sensor_type = DHT_TYPE_DHT11;
 static const gpio_num_t dht_gpio = DHT_GPIO;
+
 u8g2_t u8g2;
 QueueHandle_t bufferTemperatura; 
 QueueHandle_t bufferTemperaturaMaxima; 
-QueueHandle_t bufferTemperaturaMinima; 
+QueueHandle_t bufferTemperaturaMinima;
+QueueHandle_t bufferUmidade;
+QueueHandle_t bufferUmidadeMaxima;
+QueueHandle_t bufferUmidadeMinima;
 
 void app_main(void);
 void inicializarDisplay(void);
@@ -36,44 +43,47 @@ void task_oLED(void *pvParameters);
 
 void task_dht(void *pvParamters){
     int16_t temperatura;
-    //int16_t umidade;
-    int maxima = 0;
-    int minima = 100;
+    int16_t umidade;
+    int temperaturaMaxima   = 0;
+    int umidadeMaxima       = 0;
+    int temperaturaMinima   = 100;
+    int umidadeMinima       = 100;
 
     gpio_set_pull_mode( dht_gpio , GPIO_PULLUP_ONLY);
     
     while(1){
-        //umidade     = esp_random()/100000000;
-        temperatura = esp_random()/100000000;
+        dht_read_data(sensor_type, dht_gpio, &umidade, &temperatura);
+        umidade     = umidade / 10;
+        temperatura = temperatura / 10;
 
-        xQueueReceive(bufferTemperaturaMaxima,&maxima,pdMS_TO_TICKS(0));
-        xQueueReceive(bufferTemperaturaMinima,&minima,pdMS_TO_TICKS(0));
+        xQueueReceive(bufferTemperaturaMaxima,&temperaturaMaxima,pdMS_TO_TICKS(0));
+        xQueueReceive(bufferTemperaturaMinima,&temperaturaMinima,pdMS_TO_TICKS(0));
+        xQueueReceive(bufferUmidadeMaxima,&umidadeMaxima,pdMS_TO_TICKS(0));
+        xQueueReceive(bufferUmidadeMinima,&umidadeMinima,pdMS_TO_TICKS(0));
 
-        if (maxima <= temperatura){
-            maxima = temperatura;
+        if (temperaturaMaxima <= temperatura){
+            temperaturaMaxima = temperatura;
         }
 
-        if (minima >= temperatura){
-            minima = temperatura;
+        if (temperaturaMinima >= temperatura){
+            temperaturaMinima = temperatura;
         }
+
+        if (umidadeMaxima <= umidade){
+            umidadeMaxima = umidade;
+        }
+
+        if (umidadeMinima >= umidade){
+            umidadeMinima = umidade;
+        }        
 
         xQueueSend(bufferTemperatura,&temperatura,pdMS_TO_TICKS(0));
-        xQueueSend(bufferTemperaturaMaxima,&maxima,pdMS_TO_TICKS(0));
-        xQueueSend(bufferTemperaturaMinima,&minima,pdMS_TO_TICKS(0));
-
-        /*if(dht_read_data(sensor_type, dht_gpio, &umidade, &temperatura) == ESP_OK)
-        { 
-            umidade = umidade / 10; //Quem tem sensor
-            temperatura = temperatura / 10; //Quem tem sensor
-            xQueueSend(bufferTemperatura,&temperatura,pdMS_TO_TICKS(0)); 
-            //umidade = esp_random()/1000000;
-            //temperatura = esp_random()/1000000;
-            ESP_LOGI(TAG,"Umidade %d%% e Temperatura %dºC",umidade, temperatura );
-        }
-        else
-        {
-            ESP_LOGE(TAG, "Não foi possível ler o sensor"); 
-        } */       
+        xQueueSend(bufferTemperaturaMaxima,&temperaturaMaxima,pdMS_TO_TICKS(0));
+        xQueueSend(bufferTemperaturaMinima,&temperaturaMinima,pdMS_TO_TICKS(0));
+        xQueueSend(bufferUmidade,&umidade,pdMS_TO_TICKS(0));
+        xQueueSend(bufferUmidadeMaxima,&umidadeMaxima,pdMS_TO_TICKS(0));
+        xQueueSend(bufferUmidadeMinima,&umidadeMinima,pdMS_TO_TICKS(0));
+        
         vTaskDelay(1500/portTICK_PERIOD_MS);
     }
 }
@@ -158,13 +168,31 @@ void imprimirTemperatura(){
 }
 
 void imprimirUmidade(){
+    uint16_t umidade;
+    uint16_t maxima;
+    uint16_t minima;
+    char stringUmidade[10];
+    char stringMaxima[10];
+    char stringMinima[10];
+
     u8g2_ClearBuffer(&u8g2);
     u8g2_DrawFrame(&u8g2, 0, 0, 128, 64);
-    u8g2_DrawUTF8(&u8g2, 15, 15, "Umidade");
-    u8g2_DrawUTF8(&u8g2, EIXO_X, 60, "Max:");
-    u8g2_DrawUTF8(&u8g2, 75, 60, "Min:");
+    u8g2_DrawUTF8(&u8g2, EIXO_X, 15, "Umidade");
     
-    imprimirGrafico(15, 35, 100, "100");
+    xQueueReceive(bufferUmidade,&umidade,pdMS_TO_TICKS(2000));
+    xQueueReceive(bufferUmidadeMaxima,&maxima,pdMS_TO_TICKS(2000));
+    xQueueReceive(bufferUmidadeMinima,&minima,pdMS_TO_TICKS(2000));    
+    
+    sprintf(stringUmidade, "%d", umidade);
+    sprintf(stringMaxima, "%d", maxima);
+    sprintf(stringMinima, "%d", minima);
+
+    u8g2_DrawUTF8(&u8g2, EIXO_X, 60, "Max:");
+    u8g2_DrawUTF8(&u8g2, EIXO_X + 25, 60, stringMaxima);
+    u8g2_DrawUTF8(&u8g2, 75, 60, "Min:");
+    u8g2_DrawUTF8(&u8g2, 100, 60, stringMinima);
+
+    imprimirGrafico(EIXO_X, 25, umidade, stringUmidade);    
 }
 
 void imprimirDistancia(){
@@ -189,7 +217,10 @@ void imprimirGrafico(int eixoX, int eixoY, int valor, char porcentagem[10]){
 void app_main() {
     bufferTemperatura       = xQueueCreate(5,sizeof(uint16_t)); 
     bufferTemperaturaMaxima = xQueueCreate(5,sizeof(uint16_t)); 
-    bufferTemperaturaMinima = xQueueCreate(5,sizeof(uint16_t)); 
+    bufferTemperaturaMinima = xQueueCreate(5,sizeof(uint16_t));
+    bufferUmidade           = xQueueCreate(5,sizeof(uint16_t));
+    bufferUmidadeMaxima     = xQueueCreate(5,sizeof(uint16_t)); 
+    bufferUmidadeMinima     = xQueueCreate(5,sizeof(uint16_t)); 
 
     xTaskCreate(task_oLED,"task_oLED",2048, NULL, 1, NULL);
     xTaskCreate(task_dht,"task_dht",2048,NULL, 2, NULL);
